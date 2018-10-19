1c8ef826995fbacfc3536131ba180faac05b0a86
Oneprovider <oneprovider@sd-28357.dedibox.fr>
2018-10-19 15:06:26 +0200

    Command fill_db Добавлена команда для заполнения бд Модель - Temperature Пример команды - fill_db 2018-01-01

---
+++ api/management/commands/fill_db.py
@@ -0,0 +1,46 @@
+from django.core.management.base import BaseCommand, CommandError
+from api.models import Location, Temperature
+
+import datetime
+from datetime import datetime as dt
+import random
+
+
+class Command(BaseCommand):
+    help = 'Filling the db with temperatures'
+
+    def add_arguments(self, parser):
+        parser.add_argument('date', type=str,
+                            help='Temperatures will be created since this date every 6 hours,'
+                                 'format - YYYY-MM-DD'
+                            )
+
+    def handle(self, *args, **options):
+        date = dt.strptime(options['date'], "%Y-%m-%d")
+        locations = Location.objects.all()
+        scales = ("K", "\u2103", "\u2109")
+
+        if locations.exists():
+            locations_count = locations.__len__()
+
+            if locations_count > 1:
+                plural = True
+            else:
+                plural = False
+
+            while date < dt.now():
+                date += datetime.timedelta(hours=6)
+                rand_dig = random.uniform(-1000, 1000)
+
+                if plural:
+                    Temperature.objects.create(location=locations[int(abs(rand_dig) % locations_count)],
+                                               value=rand_dig, scale=scales[int(abs(rand_dig) % 3)],
+                                               date=date)
+                else:
+                    Temperature.objects.create(location=locations[0], value=rand_dig,
+                                               scale=scales[int(abs(rand_dig) % 3)],
+                                               date=date)
+        else:
+            print("Location doesnt exist\n"
+                  "Create locations")
+
